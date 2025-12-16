268945a2d38d80672175fb99bae3da9e546ed276
Yahya Nasir <muhammadyahyanasirmyn925@gmail.com>
2025-12-16 11:15:29 +0500

    Implement full auth, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,25 @@
-import { Slot } from 'expo-router'
+import useAuthStore from '@/store/auth.store'
+import { Redirect, Slot } from 'expo-router'
 import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
+import { images } from '../../constants'
 
-const _layout = () => {
+export default function AuthLayout() {
+  const { isAuthenticated } = useAuthStore();
+
+  if(isAuthenticated) return <Redirect href="/" />
+
   return (
-    <SafeAreaView>
-      <Text>Auth layout</Text>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+        <View style={{height: Dimensions.get('screen').height / 2.25, width: '100%',}}>
+          <ImageBackground source={images.loginGraphic} resizeMode="stretch" style={{ flex: 1, width: '100%', height: '100%' }} imageStyle={{ borderBottomLeftRadius: 12, borderBottomRightRadius: 12 }}/>
+          <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10"/>
+        </View>
+
+        
       <Slot />
-    </SafeAreaView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
-
-export default _layout
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,35 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
+import useAuthStore from '@/store/auth.store';
+import * as Sentry from '@sentry/react-native';
 import './globals.css';
+
+Sentry.init({
+  dsn: 'https://a3bec0bfacabb73eef768457d9f1211c@o4510521974652928.ingest.us.sentry.io/4510522006896640',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
+
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
+
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
     "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -22,9 +46,13 @@ export default function RootLayout() {
     }
   }, [fontsLoaded, error]);
 
-  if (!fontsLoaded) {
-    return null;
-  }
+//  if (!fontsLoaded) { return null; }
+
+  useEffect(() => {
+    fetchAuthenticatedUser()
+  }, []);
+
+  if(!fontsLoaded || isLoading) return null;
 
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+});
